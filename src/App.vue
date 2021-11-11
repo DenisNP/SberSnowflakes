f2eb879333776cee688911ce1eae3f51f2776bbe
Denis Peshekhonov <denis@peshekhonov.ru>
2021-11-12 01:21:40 +0300

    pdf save

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@
             <input type="range" min="0.5" max="9" step="0.1" v-model="cutoutsRatio"/>
             <span>{{cutoutsRatio}}</span>
             <button @click="() => generate()">Generate</button>
+            <button @click="() => generateImage()">Get PDF</button>
         </div>
         <div class="canvas-block">
             <canvas key="cv" id="canvas"/>
             <canvas key="cf" id="cFinal"/>
         </div>
+        <canvas key="ci" id="imageCanvas" width="2100" height="2970"/>
     </div>
 </template>
 
@@ -17,6 +19,7 @@
 /* eslint-disable @typescript-eslint/no-this-alias,@typescript-eslint/no-explicit-any,
 @typescript-eslint/no-empty-function */
 import Vue from 'vue';
+import { jsPDF } from 'jspdf';
 import {
     init, generate, nextStep, setup,
 } from './snowflake';
@@ -29,6 +32,7 @@ export default Vue.extend({
             bottomMargin: 50,
             sideMargin: 20,
             cutoutsRatio: 1.7,
+            triangleData: null,
         };
     },
     mounted() {
@@ -66,6 +70,51 @@ export default Vue.extend({
                 console.log(result);
                 result = nextStep(1);
             }
+        },
+        generateImage() {
+            const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+            const ci = document.getElementById('imageCanvas') as HTMLCanvasElement;
+            const ctx = ci.getContext('2d') as CanvasRenderingContext2D;
+            ctx.resetTransform();
+
+            // fill with white
+            ctx.fillStyle = '#ffffff';
+            ctx.fillRect(0, 0, ci.width, ci.height);
+
+            // prepare
+            const scale = ci.width / (canvas.height * 2);
+            ctx.scale(scale, scale);
+            const startX = (ci.width / scale) / 2 - canvas.width / 2;
+            const nextX = startX + canvas.width;
+
+            // draw triangle
+            ctx.lineWidth = 2;
+            ctx.fillStyle = '#dedede';
+            ctx.strokeStyle = '#eeeeee';
+            ctx.beginPath();
+            ctx.moveTo(startX, 0);
+            ctx.lineTo(nextX, 0);
+            ctx.lineTo(startX + canvas.width / 2, canvas.height);
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
+
+            // cut out
+            ctx.drawImage(canvas, startX, 0);
+
+            // draw line for help
+            ctx.strokeStyle = '#000000';
+            ctx.setLineDash([10, 10]);
+            ctx.beginPath();
+            ctx.moveTo(0, ci.width / scale);
+            ctx.lineTo(ci.width / scale, ci.width / scale);
+            ctx.stroke();
+
+            // create pdf
+            // eslint-disable-next-line new-cap
+            const doc = new jsPDF();
+            doc.addImage(ci.toDataURL('image/jpeg'), 0, 0, 210, 297);
+            doc.save('snowflake.pdf');
         },
     },
 });
@@ -108,6 +157,11 @@ root {
     margin: calc(var(--final-canvas-size) * -0.25);
 }
 
+#imageCanvas {
+    position: fixed;
+    display: none;
+}
+
 .canvas-block {
     max-width: 100vw;
     max-height: 100vh;
@@ -133,6 +187,10 @@ root {
     color: white;
     padding: 5px 10px;
     border-radius: 3px;
+}
+
+.buttons-block button:last-child {
+    margin-left: 5px;
 }
 
 .buttons-block span {
